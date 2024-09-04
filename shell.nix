56e4ea5cdd5e86b14fba418fb4bb8ad99e5a6743
Yaohua Xiong <xiongyaohua@gmail.com>
2024-09-04 09:39:55 +0800

    nix-shell环境

---
+++ shell.nix
@@ -0,0 +1,22 @@
+# 供nixos环境下使用
+# 用法：
+# 1. 命令行切换到当前目录
+# 2. nix-shell，进入开发环境
+# 3. jupyter server，启动jupyter服务器
+# 4. 在vscode里面连接该服务器
+#
+# TODO：调查为什么直接从vscode里面启动服务器会出错。
+
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: [
+      python-pkgs.numpy
+      python-pkgs.pygame
+      python-pkgs.matplotlib
+      python-pkgs.jupyter
+      python-pkgs.notebook
+    ]))
+  ];
+}
